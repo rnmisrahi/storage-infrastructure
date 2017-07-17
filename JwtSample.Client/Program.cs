912eac2deff22897e01d48b0af301243d0f2c0e0
HOME\misha <andreyevmb@live.ru>
2017-07-17 23:00:57 +0500

    Add .NET Console Client

---
+++ JwtSample.Client/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace JwtSample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Server url with backslash (http://46.146.232.216/JwtSample/): ");
+            string url = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                url = "http://46.146.232.216/JwtSample/";
+            }
+
+            Task<TokenInfo> getTokenTask = null;
+            try
+            {
+                getTokenTask = GetTokenAsync(new Uri(new Uri(url), "token"), "RubenMisrahi");
+                getTokenTask.Wait();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid User ID or endpoint address.");
+            }
+
+            if (getTokenTask != null)
+            {
+                string jwt = getTokenTask.Result.access_token;
+
+                Console.WriteLine($"Your token is {jwt}");
+
+                Task<string> addChildTask = null;
+
+                try
+                {
+                    addChildTask = AddChildAsync(new Uri(new Uri(url), "api/data/childs"), jwt, "Anna", "2017-01-01");
+                    addChildTask.Wait();
+                }
+                catch
+                {
+                    Console.WriteLine("Error on adding child.");
+                }
+
+                if (addChildTask != null)
+                {
+                    Console.WriteLine(addChildTask.Result);
+                }
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Get JWT token
+        /// </summary>
+        /// <param name="uri">Endpoint URI</param>
+        /// <param name="personId">Person ID</param>
+        /// <returns>Token Info</returns>
+        static async Task<TokenInfo> GetTokenAsync(Uri uri, string personId)
+        {
+            // Create HTTP Client
+            HttpClient client = new HttpClient();
+
+            // Add parameters
+            HttpContent content = new StringContent($"personid={personId}");
+            // Add Content-Type header
+            content.Headers.ContentType.MediaType = "application/x-www-form-urlencoded";
+
+            // Send request to server
+            HttpResponseMessage response = await client.PostAsync(uri, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // if Success the return Token Info
+                return await response.Content.ReadAsAsync<TokenInfo>();
+            }
+            else
+            {
+                throw new Exception("Not authorized");
+            }
+        }
+
+        /// <summary>
+        /// Add child
+        /// </summary>
+        /// <param name="uri">Endpoint URI</param>
+        /// <param name="jwt">JWT token</param>
+        /// <param name="childName">Child Name</param>
+        /// <param name="birthDate">Birth Date</param>
+        /// <returns>Text result from server</returns>
+        static async Task<string> AddChildAsync(Uri uri, string jwt, string childName, string birthDate)
+        {
+            // Create HTTP Client
+            HttpClient client = new HttpClient();
+            // Add authorization Header with JWT token
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+
+            // Add parameters
+            HttpContent content = new StringContent($"childName={childName}&birthDate={birthDate}");
+            // Add Content-Type header
+            content.Headers.ContentType.MediaType = "application/x-www-form-urlencoded";
+
+            // Send request to server
+            HttpResponseMessage response = await client.PostAsync(uri, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // If Success then return text result from server
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new Exception("Not authorized");
+            }
+        }
+    }
+}
---
+++ JwtSample.Client/TokenInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JwtSample.Client
+{
+    public class TokenInfo
+    {
+        public string access_token { get; set; }
+        public string username { get; set; }
+    }
+}
